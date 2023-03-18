9b5896eb563d0ace696855d7dd2f39a0691b49b9
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-18 23:05:27 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/** main - c programming
+ * Return 0
+ */
+int main(void)
+{
+/*declare an alphabeth */
+char =alphabeth;
+
+for(alphabeth=a; alphabeth<=z; alphabeth++)
+{	
+putchar(alphabet);
+}
+
+putchar("\n")
+
+return (0);
+}
