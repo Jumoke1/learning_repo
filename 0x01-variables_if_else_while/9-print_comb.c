2f5a5160b5af058c4c8b1eeedfd626c38814c066
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-19 16:55:09 +0100

    print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**main c programming
+ * Return 0
+ */
+int main(void)
+{
+int i;
+for(i=48; i<58; i++)
+{
+putchar(i)
+}
+if(n!=57)
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
