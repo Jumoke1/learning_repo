920f3f63f8d20418a90fc459482c8c7db1135059
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-19 16:42:14 +0100

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**main c programming
+ *Return 0
+ */
+int main(void)
+{
+int i ;
+char n;
+for(i=48; i<57; i++)
+{
+putchar(i);
+}
+for(n='a'; n<='f'; n++)
+{
+putchar(n);
+}	
+putchar('\n')
+return (0);
+}
