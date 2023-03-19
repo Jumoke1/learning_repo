6f1519048638ee4ef61978ec44d5c30ec0514ae1
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-19 12:49:38 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*betty style*/
+int main(void)
+
+{
+char = T;
+for(T=a; T<=Z; T++);
+{
+
+putchar(T);	
+}
+putchar("/n");
+return(0);
+}
+
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/** main c programming/
+ * Return 
+ */
+int main(void)
+{
+char ct -"a";
+
+while(ct<= z)
+{
+if(ct !="q" && ct !="e")
+{
+putchar(ct)
+}
+ct++
+
+}
+putchar("/");
+
+return(0);
+}
